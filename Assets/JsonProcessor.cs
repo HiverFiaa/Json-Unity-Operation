6a20ae77b6f0d8e0b89aa43a117aa7932dcc0ce0
HiverFiaa <50221885+HiverFiaa@users.noreply.github.com>
2019-06-06 19:02:31 +0800

    Initial commit

---
+++ Assets/JsonProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonProcessor : MonoBehaviour {
+
+	public EnvironmentalMonitoring envirdata;
+    private float time;
+
+	// Use this for initialization
+	void Start () {
+		envirdata = new EnvironmentalMonitoring ();
+        time = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (time > 10)
+        {
+            updateEnvirData();
+            Debug.Log(JsonUtility.ToJson(envirdata)+"\n______________________\n"+JsonUtility.FromJson<EnvironmentalMonitoring>(JsonUtility.ToJson(envirdata)).dataType);
+
+        }
+	}
+
+	void updateEnvirData(){
+        envirdata.dataType = true ? "initial" : null;
+
+	}
+
+
+}
+
+[System.Serializable]
+public class EnvironmentalMonitoring
+{
+	public string dataType;
+	public float temperature, humidity, PM25, CO, PM10, NO2, SO2, O3, waterLevel, rainfallIntensity;
+
+
+}
